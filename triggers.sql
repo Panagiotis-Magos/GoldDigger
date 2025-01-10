dee9016692466c151c49b74cee19e10a39c766da
irenekap <118299247+irenekap@users.noreply.github.com>
2025-01-10 15:51:49 +0200

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -5,7 +5,7 @@ FOR EACH ROW
 BEGIN
   IF NEW.is_completed = 1 AND OLD.is_completed = 0 THEN
     UPDATE `usergoals` ug
-    SET ug.tasks_completed = ug.tasks_completed + 1
+    SET ug.progress = ug.progress + 1
     WHERE ug.user_id = NEW.user_id
       AND ug.goal_id IN (
         SELECT gt.goal_id
@@ -22,7 +22,7 @@ FOR EACH ROW
 BEGIN
   IF NEW.is_completed = 0 AND OLD.is_completed = 1 THEN
     UPDATE `usergoals` ug
-    SET ug.tasks_completed = ug.tasks_completed - 1
+    SET ug.progress = ug.progress - 1
     WHERE ug.user_id = NEW.user_id
       AND ug.goal_id IN (
         SELECT gt.goal_id
@@ -76,6 +76,30 @@ BEGIN
   WHERE `goal_id` = OLD.goal_id;
 END;
 //
+
+CREATE TRIGGER update_gold
+AFTER INSERT ON `usertasks`
+FOR EACH ROW
+BEGIN
+  -- Ensure the task is marked as completed
+  IF NEW.is_completed = 1 THEN
+    -- Update the user's gold by adding the task's gold_reward
+    UPDATE `users`
+    SET `gold` = `gold` + (
+      SELECT `gold_reward`
+      FROM `tasks`
+      WHERE `task_id` = NEW.task_id
+    )
+    WHERE `user_id` = NEW.user_id;
+  END IF;
+END;
+//
+
+
+
+
+
+
 
 
 
