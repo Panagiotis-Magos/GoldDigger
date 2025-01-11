ab6b578af8cdd1dd5d72cdb02ca1f78eee6f72d7
mariadrosou <mariadrosou123@gmail.com>
2025-01-12 00:36:34 +0200

    Update app_routes.dart

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -3,27 +3,21 @@ import '../screens/intro.dart';
 import '../screens/editprofile.dart';
 import '../screens/login.dart';
 import '../screens/profile.dart';
+import '../screens/mystuff.dart';
 import '../utils/appstate.dart';
 
 class AppRoutes {
-  static const String home = '/home';
-  static const String login = '/login';
-  static const String editprofile = '/editprofile';
   static const String intro = '/';
   static const String profile = '/profile';
   static const String mystuff = '/mystuff';
+  static const String editprofile = '/editprofile';
+  static const String login = '/login';
+
   static final Map<String, WidgetBuilder> routes = {
-    home: (context) => Scaffold(
-            appBar: AppBar(
-              title: const Text('Home Screen Placeholder'),
-            ),
-            body: const Center(
-              child: Text('Welcome to the Home Screen!'),
-            ),
-          ),
     intro: (context) => IntroScreen(),
     login: (context) => LoginScreen(),
-    profile: (context) => ProfileScreen(userId:  AppState().globaluserId),
-    editprofile: (context) => EditProfileScreen(),
+    profile: (context) => ProfileScreen(userId: AppState().globaluserId),
+    editprofile: (context) => EditProfileScreen(userId: AppState().globaluserId),
+    mystuff: (context) => MyStuffScreen(userId: AppState().globaluserId),
   };
 }
